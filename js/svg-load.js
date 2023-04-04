f5a765ba73a0b41582603026e28a024fbf9f6019
gcastill0 <gilberto@hashicorp.com>
2023-04-04 15:18:38 -0400

    Adding relevant path

--- js/svg-load.js
+++ js/svg-load.js
@@ -4,7 +4,7 @@ window.onload = function () {
     const parser = new DOMParser();
 
     async function fetchSVG() {
-        const response = await fetch('/img/undraw_happy_music_g6wc.svg');
+        const response = await fetch('img/undraw_happy_music_g6wc.svg');
         const svgText = await response.text();
         const svgDoc = parser.parseFromString(svgText, 'text/xml');
         svg_diagram.appendChild(svgDoc.documentElement);
